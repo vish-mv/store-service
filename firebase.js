b54e50c2695fe8c81cd513987e9fca5a5123540e
Visal Munasinghe <127712545+vish-mv@users.noreply.github.com>
2025-08-26 14:38:20 +0530

    update config mismatch

--- firebase.js
+++ firebase.js
@@ -1,10 +1,12 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-key.json");
+const fs = require('fs');
+let keyPath = '/firebase-key.json';
+
+if (!fs.existsSync(keyPath)) {
+  keyPath = './firebase-key.json'; // fallback for local dev
+}
+
+const serviceAccount = require(keyPath);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-
-const db = admin.firestore();
-
-module.exports = db;
